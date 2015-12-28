989d6cf51b54f99147280e5a3c631fac24a3a95c
Daniel Munoz <lostlucidity@gmail.com>
2015-12-28 11:48:27 -0500

    Created admin and home controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/new'
+
+  get 'signup' => 'admins#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
