4e869902ca98cc59bdffc1bcfed3ca4998cf54b0
Daniel Munoz <lostlucidity@gmail.com>
2016-01-08 18:04:39 -0500

    email validation using regex, wyncode.co specific.

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -29,4 +29,22 @@ class AdminTest < ActiveSupport::TestCase
   	@admin.email = "a" * 245 + "@wyncode.co"
   	assert_not @admin.valid?
   end
+
+  test "email validation should accept valid addresses" do
+  	valid_addresses = %w[user@wyncode.co USER@wyncode.co A_US-ER@wyncode.co 										 first.last@wyncode.co alice+bob@wyncode.co]
+  	valid_addresses.each do |valid_address|
+  		@admin.email = valid_address
+  		assert @admin.valid? "#{valid_address.inspect} should be valid"
+  	end
+  end
+
+  test "email validation should reject invalid addresses" do
+  	invalid_addresses = %w[user@wyncode,co user_at_wyncode.co 
+  												 user.name@wyncode. foo@wyn_code.co foo@wyn+code.co
+  												 user@example.co]
+  	invalid_addresses.each do |invalid_address|
+  		@admin.email = invalid_address
+  		assert_not @admin.valid? "#{invalid_address.inspect} should be invalid"
+  	end
+  end
 end
