fa0c9c96bbfa6cbdd4ae88d5f036669576bcb5ca
Daniel Munoz <lostlucidity@gmail.com>
2016-01-11 16:44:08 -0500

    Add password_digest to admin model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111212519) do
+ActiveRecord::Schema.define(version: 20160111214225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20160111212519) do
   create_table "admins", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
